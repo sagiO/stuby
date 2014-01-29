1415f06c3d33bf12388f10041858f099fd906c95
sagiO <ba06212@gmail.com>
2014-01-29 23:15:46 +0900

    first commit

---
+++ Gemfile
@@ -0,0 +1,22 @@
+source 'https://rubygems.org'
+gem 'rails', '4.0.2'
+gem 'mysql2', '~> 0.3.14'
+gem 'sass-rails', '~> 4.0.0'
+gem 'uglifier', '>= 1.3.0'
+gem 'coffee-rails', '~> 4.0.0'
+gem 'therubyracer', platforms: :ruby
+gem 'jquery-rails'
+gem 'turbolinks'
+gem 'jbuilder', '~> 1.2'
+
+group :doc do
+  gem 'sdoc', require: false
+end
+
+group :development, :test do
+	gem 'better_errors'
+	gem 'binding_of_caller'
+	gem 'quiet_assets'
+end
+
+gem 'devise'
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Stuby::Application.config.secret_key_base = '7a4a2990e44dfce35124ee04a44a35963f862e4b6868020baa619011f0b946be195f1277cc705a90670edbb0f4dc97386eee954e2639ed6e8839fe4f59b994ab'
